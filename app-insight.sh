da68e0b63c76f5d984ab4d59f2b8400140a2ed47
RohanAwasthi <128706839+RohanAwasthi@users.noreply.github.com>
2024-10-09 17:42:15 +0530

    Initial commit

---
+++ app-insight.sh
@@ -0,0 +1,23 @@
+webAppName='demo004'
+resourceGroupName='devex-demo004-rg'
+appName='demo004'
+location="eastus"
+
+appInsights=$(az monitor app-insights component create \
+  --app $appName \
+  --location $location \
+  --resource-group $resourceGroupName \
+  --application-type web \
+  --query instrumentationKey -o tsv)
+
+appInsightsConnection=$(az monitor app-insights component create \
+  --app $appName \
+  --resource-group $resourceGroupName \
+  --query connectionString -o tsv)
+echo "Application Insights resource created with Instrumentation Key: $appInsights"
+# Link Application Insights to the Web App
+echo "Linking Application Insights to Web App..."
+az webapp config appsettings set \
+  --name $webAppName \
+  --resource-group $resourceGroupName \
+  --settings APPINSIGHTS_INSTRUMENTATIONKEY=$appInsights APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 APPLICATIONINSIGHTS_CONNECTION_STRING=$appInsightsConnection ApplicationInsightsAgent_EXTENSION_VERSION=~2
---
+++ terraform.tfvars
@@ -0,0 +1,10 @@
+# name    = "demo004"
+location = "east us"
+sku_name = "D1"
+os_type = "Linux"
+tags = {
+  "pid" = "demo004"
+  "project" = "dev-ex"
+  "env" = "demo"
+
+}
